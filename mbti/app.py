cc2ea3c1c6ca66ca9e2384f6a135babe5039241a
maitracle <maitracle@gmail.com>
2020-08-13 17:33:15 +0900

    change secret key for jwt

--- mbti/app.py
+++ mbti/app.py
@@ -13,10 +13,8 @@ app.register_blueprint(mbti_matcher_views)
 client = MongoClient('localhost', 27017)
 db = client.db_mbti
 
-app.config['JWT_SECRET_KEY'] = 'secret-key-need'  # Change this!
+app.config['JWT_SECRET_KEY'] = 'g#*5q+!l0tglppmfq9gq-h7w3o(7te**j8jjq8rsqad^&nbjcd'  # Change this!
 jwt = JWTManager(app)
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
-
-# asdf
